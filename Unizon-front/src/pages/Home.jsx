55aed29f32d1ba22c9599db1eabfbd1a414435b0
Kassigg <kassi.shef@gmail.com>
2023-08-05 04:56:03 +0300

    getting tags from backend+

--- Unizon-front/src/pages/Home.jsx
+++ Unizon-front/src/pages/Home.jsx
@@ -14,7 +14,9 @@ import { fetchTags } from '../redux/slices/posts';
 export const Home = () => {
   const dispatch = useDispatch();
   const {posts, tags} = useSelector(state => state.posts);
+
   const isPostsLoading = posts.status == 'loading';
+  const isTagsLoading = tags.status == 'loading';
 
   React.useEffect(() => {
     dispatch(fetchPosts());
@@ -50,7 +52,7 @@ export const Home = () => {
         )}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
