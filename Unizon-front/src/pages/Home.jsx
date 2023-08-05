1efc84a20fe3fbb4b856b61c96eb59d8d6040b92
Kassigg <kassi.shef@gmail.com>
2023-08-05 03:58:08 +0300

    add-asyncThunk

--- Unizon-front/src/pages/Home.jsx
+++ Unizon-front/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tabs from '@mui/material/Tabs';
+import { useDispatch } from 'react-redux';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
 
@@ -7,10 +8,14 @@ import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
 import axios from '../axios';
+import { fetchPosts } from '../redux/slices/posts';
 
 export const Home = () => {
+  const dispatch=useDispatch();
+
   React.useEffect(() => {
-    axios.get('/posts');
+    dispatch(fetchPosts());
+
   }, []);
 
   return (
@@ -35,6 +40,7 @@ export const Home = () => {
               viewsCount={150}
               commentsCount={3}
               tags={['react', 'fun', 'typescript']}
+              isLoading={true}
               isEditable
             />
           ))}
