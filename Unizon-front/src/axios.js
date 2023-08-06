be8fa4d9a61ffed31653090474c4ddb92f4ce00a
KashAlOt4SV <shaya.ashot@mail.ru>
2023-08-06 16:44:57 +0300

    Add the auth and add the auth.token

--- Unizon-front/src/axios.js
+++ Unizon-front/src/axios.js
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://localhost:5555'
-})
+});
+
+instance.interceptors.request.use((config) => {
+    config.headers.Authorization = window.localStorage.getItem('token');
+
+    return config;
+});
 
 export default instance;
