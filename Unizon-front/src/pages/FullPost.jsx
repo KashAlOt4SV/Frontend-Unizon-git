42c5580ae76ab6e0cf3e69bf6e191c89b1a93c9d
Kassigg <kassi.shef@gmail.com>
2023-08-12 21:44:23 +0300

    Full post

--- Unizon-front/src/pages/FullPost.jsx
+++ Unizon-front/src/pages/FullPost.jsx
@@ -9,18 +9,19 @@ import { CommentsBlock } from "../components/CommentsBlock";
 export const FullPost = () => {
   const [data, setData] = React.useState();
   const [isLoading, setLoading] = React.useState(true);
-  const {id} = useParams();
+  const { id } = useParams();
 
 React.useEffect(() => {
   axios
-    .get('/posts/${id}')
-    .then(res => {
+    .get(`/posts/${id}`)
+    .then((res) => {
       setData(res.data);
+      setLoading(false);
     })
-    .catch(err => {
+    .catch((err) => {
       console.warn(err);
       alert('Ошибка при получении статьи');
-    });
+    })
 }, []);
 
 if (isLoading) {
@@ -32,7 +33,8 @@ if (isLoading) {
       <Post
         id={data._id}
         title={data.title}
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+        imageUrl={data.imageUrl}
+        //imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
