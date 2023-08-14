ce5b9588a125faeecb0ed2300957774b91a46cc8
Kassigg <kassi.shef@gmail.com>
2023-08-14 17:16:32 +0300

    Edit Posts

--- Unizon-front/src/pages/AddPost/index.jsx
+++ Unizon-front/src/pages/AddPost/index.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
-import { useNavigate, Navigate } from "react-router-dom";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
 import axios from '../../axios'
 
 import 'easymde/dist/easymde.min.css';
@@ -12,14 +12,17 @@ import styles from './AddPost.module.scss';
 import { selectIsAuth } from "../../redux/slices/auth";
 
 export const AddPost = () => {
+  const {id} = useParams();
   const navigate = useNavigate();
   const isAuth = useSelector(selectIsAuth);
   const [isLoading, setLoading] = React.useState(false);
   const [text, setText] = React.useState('');
   const [title, setTitle] = React.useState('');
-  const [imageUrl, setimageUrl] = React.useState('');
+  const [imageUrl, setImageUrl] = React.useState('');
   const [tags, setTags] = React.useState('');
   const inputFileRef = React.useRef(null);
+
+  const isEditing = Boolean(id);
 
   const handleChangeFile = async(event) => {
     try {
@@ -27,7 +30,7 @@ export const AddPost = () => {
       const file = event.target.files[0];
       formData.append('image', file);
       const { data } = await axios.post('/upload', formData);
-      setimageUrl(data.url);
+      setImageUrl(data.url);
     } catch (err) {
       console.warn(err);
       alert('Ошибка при загрузке файла!')
@@ -35,7 +38,7 @@ export const AddPost = () => {
   };
 
   const onClickRemoveImage = () => {
-    setimageUrl('');
+    setImageUrl('');
   };
 
   const onChange = React.useCallback((value) => {
@@ -51,17 +54,35 @@ export const AddPost = () => {
         tags,
         text
       };
-      const {data} = await axios.post('/posts', fields);
+      
+      const {data} = isEditing 
+      ? await axios.patch(`/posts/${id}`, fields)
+      : await axios.post('/posts', fields);
 
-      const id = data._id;
+      const _id = isEditing ? id : data._id;
 
-      navigate(`/posts/${id}`);
+      navigate(`/posts/${_id}`);
 
     } catch (err) {
       console.warn(err);
       alert('Ошибка при создании статьи!');
     }
   };
+
+  React.useEffect(() => {
+    if (id) {
+      axios.get(`/posts/${id}`)
+      .then(({data}) => {
+        setTitle(data.title);
+        setText(data.text);
+        setImageUrl(data.imageUrl);
+        setTags(data.tags.join(','));
+      }).catch((err) => {
+        console.warn(err);
+        alert('Ошибка при получении статьи!');
+      })
+    }
+  }, []);
 
   const options = React.useMemo(
     () => ({
@@ -116,7 +137,7 @@ export const AddPost = () => {
       <SimpleMDE className={styles.editor} value={text} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button onClick={onSubmit} size="large" variant="contained">
-          Опубликовать
+          {isEditing ? 'Сохранить' : 'Опубликовать'}
         </Button>
         <a href="/">
           <Button size="large">Отмена</Button>
