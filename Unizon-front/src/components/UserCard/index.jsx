838b0beb7f0b4d869a6c4197c07c42a88e5fdf88
Kassigg <kassi.shef@gmail.com>
2023-08-23 17:45:41 +0300

    part of userCard

--- Unizon-front/src/components/UserCard/index.jsx
+++ Unizon-front/src/components/UserCard/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './UserCard.module.scss';
+import Button from '@mui/material/Button';
+import axios from '../../axios'
 
 export const UserCard = ({
     avatarUrl, 
@@ -8,18 +10,37 @@ export const UserCard = ({
     interests,
 }) => {
 
+    const inputFileRef = React.useRef(null);
+    const [imageUrl, setImageUrl] = React.useState('');
+
+    const handleChangeFile = async(event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append('image', file);
+      const { data } = await axios.post('/upload', formData);
+      const { avatarUrl } = await axios.patch('/updateAvatar', avatarUrl);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert('Ошибка при загрузке файла!')
+    }
+  };
+
     
 
     return (
         <div className={styles.root}>
             <img className={styles.avatar} src={avatarUrl ? `http://localhost:5555${avatarUrl}` :  '/noavatar.png'} alt={fullName} />
             <div className={styles.userDetails}>
-                <span className={styles.userName}>{fullName}</span>
-                <em className={styles.typeOfUser}>{typeOfUser}</em>
+                <span className={styles.userName}>{fullName}</span> 
             </div>
-            <div>
+            <div className={styles.userInfo}>
                 <h3 className={styles.aboutMe}>Обо мне:</h3>
                 <span className={styles.interests}>{interests}</span>
+            </div>
+            <div>
+                <em className={styles.typeOfUser}>{typeOfUser}</em>
             </div>
         </div>
     )
