15be087f8d6a78246bf5e2699f1d57603aa484e1
KashAlOt(4SV) <shaya.ashot@mail.com>
2024-09-02 22:39:03 +0300

    test commit 2

--- Unizon-front/src/pages/Home.jsx
+++ Unizon-front/src/pages/Home.jsx
@@ -72,7 +72,7 @@ export const Home = () => {
                   fullName: 'Вася Пупкин',
                   avatarUrl: 'https://mui.com/static/images/avatar/1.jpg',
                 },
-                text: 'Это тестовый комментарий1',
+                text: 'Это тестовый комментарий',
               },
               {
                 user: {
